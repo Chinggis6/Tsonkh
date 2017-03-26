af375e484448ef6b48bb4ba339119e07562946a8
Chinggis Tenger <Chinggis6@iCloud.com>
2017-03-26 15:20:06 +0400

    Bulk commit

--- Windows.bat
+++ Windows.bat
@@ -1,5 +1,7 @@
 rem C:\Users\Administrator\AppData\Local
 rem setx C %LocalAppData%
+rem setx home %UserProfile%
+rem setx H %home%
 rem explorer %LocalAppData%
 
 rem doskey cin=choco install -y --ignore-checksums $1
